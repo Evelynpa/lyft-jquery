fafe6a323ec4dc874e3a638ff549d605b666dde3
Evelynpa <evelyn.parra.rojas@gmail.com>
2017-12-14 17:53:58 -0300

    se realiza pagina Lyft

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,4 +1,17 @@
 $(document).ready(function () {
     $('.splash').fadeOut(5000);
+    var number = $('#number').val();
+    // var max = 11;
+    // function getRandomArbitrary(number, max) {
+    //     console.log( Math.random() * (max - number) + number);
+    // }
+    $('#number').keyup(function () {
+        if(number != 0){
+            $('#btnNext').attr("disabled", true);
+        }else{
+            $('#btnNext').attr("disabled", false);
+        }
+
+    });
 
 })
